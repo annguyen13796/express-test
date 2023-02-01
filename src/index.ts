e03954704e76bfcfead0b6445e7d542833c7ed95
annguyen13796 <nguyenlaman.1307@gmail.com>
2023-02-01 22:50:30 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-import { Application } from './app';
+import express, { Express } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-const app = new Application();
-app.startServer();
+// import dotenv from 'dotenv';
+// dotenv.config({ path: './config/config.env' });
+// console.log(process.env);
+import { applicationRouter } from './routers';
+export const app: Express = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(applicationRouter);
+
+app.listen(5000, () => {
+  console.log('server run');
+});
